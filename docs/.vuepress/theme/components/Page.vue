d40a3e5a8d644b22edc4389d45a5eb4691ccb723
gsk567 <georgi.karagogov@gmail.com>
2021-02-27 01:41:10 +0200

    refactor styles - remove stylus dependencies

--- docs/.vuepress/theme/components/Page.vue
+++ docs/.vuepress/theme/components/Page.vue
@@ -22,7 +22,6 @@ export default {
 </script>
 
 <style lang="stylus">
-@require '../styles/wrapper.styl'
 
 .page
   padding-bottom 2rem
--- docs/.vuepress/theme/global-components/Badge.vue
+++ docs/.vuepress/theme/global-components/Badge.vue
@@ -22,23 +22,3 @@ export default {
   }
 }
 </script>
-
-<style lang="stylus" scoped>
-.badge
-  display inline-block
-  font-size 14px
-  height 18px
-  line-height 18px
-  border-radius 3px
-  padding 0 6px
-  color white
-  background-color #42b983
-  &.tip, &.green
-    background-color $badgeTipColor
-  &.error
-    background-color $badgeErrorColor
-  &.warning, &.warn, &.yellow
-    background-color $badgeWarningColor
-  & + &
-    margin-left 5px
-</style>
